e49a7a8f880298428fb189b78af2e5a436301348
Jeremy Carbaugh <jeremy@isl.co>
2016-02-10 15:39:26 -0500

    serve public static files

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/wsgi.py
@@ -8,10 +8,14 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.package_name }}.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "{{ cookiecutter.package_name }}.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+public_path = os.path.join(os.path.dirname(__file__), 'public')
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+application.add_files(public_path, prefix='/')
