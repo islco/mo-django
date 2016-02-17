df70099d6de1b8e8355d86a48801f2e4ad901ad8
Jeremy Carbaugh <jeremy@isl.co>
2016-02-17 15:22:40 -0500

    move public directory to project level

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/wsgi.py
@@ -14,7 +14,8 @@ os.environ.setdefault(
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-public_path = os.path.join(os.path.dirname(__file__), 'public')
+public_path = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), 'public')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
