55743c1c0daf775aae439aab8fd37ea6259726b5
Joseph Solomon <joseph.solomon89@gmail.com>
2017-04-04 15:54:00 -0400

    add the storage too

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/storage.py
@@ -0,0 +1,12 @@
+from django.contrib.staticfiles import finders
+from django.conf import settings
+
+from whitenoise.django import GzipManifestStaticFilesStorage
+
+class DebugErroringGzipManifestStaticFilesStorage(GzipManifestStaticFilesStorage):
+
+    def url(self, name):
+        if settings.DEBUG:
+            if finders.find(name) is None:
+                raise ValueError("The file '%s' could not be found with %r." % (name, self))
+        return super(DebugErroringCompressedManifsetStaticFileStorage, self).url(name)
