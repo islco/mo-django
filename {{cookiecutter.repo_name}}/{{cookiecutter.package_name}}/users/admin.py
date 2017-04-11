df45369d1a66825ddd2f68696be8c7c85bb4632d
Joseph Solomon <joseph.solomon89@gmail.com>
2017-04-11 17:16:22 -0400

    add User to the admin (#77)

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/users/admin.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UAdmin):
+    pass
