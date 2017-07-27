fb1bf5e6d4239775805c9761e2e5f82f8ed5e088
Joseph Solomon <joseph.solomon89@gmail.com>
2017-07-27 15:33:40 -0400

    include media serving in debug mode

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+    from django.conf.urls.static import static
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
