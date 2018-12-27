c0627f1b31b09396266140140a0567f8165e7ef5
cornelius <cornelius@isl.co>
2018-12-27 17:01:51 -0500

    bump versions and make compatiable

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/wsgi.py
@@ -3,11 +3,5 @@ os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "{{ cookiecutter.package_name }}.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-public_path = os.path.join(
-    os.path.dirname(os.path.dirname(__file__)), 'public')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-application.add_files(public_path, prefix='/')
